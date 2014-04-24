af8ff1380d867868bee35cb188cb58bf5fdd701d
Scott Fleckenstein <nullstyle@gmail.com>
2014-04-24 15:12:07 -0700

    Adds stubbed login page

--- app/scripts/app-controller.js
+++ app/scripts/app-controller.js
@@ -11,3 +11,24 @@ sc.controller('NavCtrl', function($scope) {
 
   $scope.loggedIn = false;
 });
+
+sc.controller('LoginCtrl', function($scope, $state) {
+  $scope.email      = null;
+  $scope.password   = null;
+  $scope.loginError = null;
+
+  $scope.attemptLogin = function() {
+    //TODO: hash up the passwords
+    //      locate the user blob and decrypt
+    //      go to dashboard
+
+    if($scope.email === "success@example.com") {
+      // this is the dummy success state
+      $state.go("dashboard");
+    } else {
+      // this is the dummy failure state
+      // TODO: we should tell them _why_ they failed to login here
+      $scope.loginError = "Error!"
+    }
+  }
+});
