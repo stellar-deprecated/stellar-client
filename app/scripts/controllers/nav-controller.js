b48bab8e22a543cdee1cab8e64441ad6f11d0385
Jared Deckard <jared.deckard@gmail.com>
2014-05-12 11:08:04 -0700

    Seperate and organize controllers, fix README.

---
+++ app/scripts/controllers/nav-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var sc = angular.module('stellarClient');
+
+sc.controller('NavCtrl', function($scope, session) {
+  // The session is initially not logged in.
+  session.put('loggedIn',  false);
+
+  // Allow the nav to access the session variables.
+  $scope.session = session;
+});
