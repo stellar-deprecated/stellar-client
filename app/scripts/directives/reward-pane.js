333cbacc04980f8c1537faeedfbe7bc89fabd25b
Jared Deckard <jared.deckard@gmail.com>
2014-05-16 18:10:04 -0700

    Start rewards logic, hard code user rewards, quick styling pass

--- app/scripts/directives/reward-pane.js
+++ app/scripts/directives/reward-pane.js
@@ -2,15 +2,32 @@
 
 var sc = angular.module('stellarClient');
 
-sc.directive('rewardPane', function(session, ngTableParams, $filter){
-    return {
-        restrict: 'E',
-        replace: true,
-        transclude: false,
-        scope: {},
-        templateUrl: '/templates/reward-pane.html',
-        controller: function($scope, $element){
+sc.directive('rewardPane', function (session, ngTableParams, $filter) {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: false,
+    scope: {},
+    templateUrl: '/templates/reward-pane.html',
+    controller: function ($scope, $element) {
+      $scope.incompleteRewards = [];
+      $scope.completeRewards = [];
 
-        }
-    };
+      // TODO: Load the reward types from the RewardTypes table.
+      var rewardTypes = [
+        {title: 'facebook', message: 'Verify your identity by connecting with Facebook'},
+        {title: 'email', message: 'Enable account recovery by verifying your email address'},
+        {title: 'send', message: 'Send stellars to someone'},
+        {title: 'wallet', message: 'Create a new wallet'}
+      ];
+
+      // TODO: Load the user's completed rewards from the RewardsGiven table.
+      var rewardsGiven = ['wallet'];
+
+      rewardTypes.forEach(function(reward){
+        if(rewardsGiven.indexOf(reward.title) == -1) $scope.incompleteRewards.push(reward);
+        else $scope.completeRewards.push(reward);
+      })
+    }
+  };
 });
