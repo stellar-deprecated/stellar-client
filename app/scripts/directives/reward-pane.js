af6eec4cefbc18095e391e449c43f66dbf2fc2e4
Jared Deckard <jared.deckard@gmail.com>
2014-05-19 14:12:56 -0700

    Load the user's reward status, add status icons

--- app/scripts/directives/reward-pane.js
+++ app/scripts/directives/reward-pane.js
@@ -2,32 +2,59 @@
 
 var sc = angular.module('stellarClient');
 
-sc.directive('rewardPane', function (session, ngTableParams, $filter) {
+sc.directive('rewardPane', function () {
   return {
     restrict: 'E',
     replace: true,
     transclude: false,
     scope: {},
     templateUrl: '/templates/reward-pane.html',
-    controller: function ($scope, $element) {
-      $scope.incompleteRewards = [];
-      $scope.completeRewards = [];
 
-      // TODO: Load the reward types from the RewardTypes table.
-      var rewardTypes = [
-        {title: 'facebook', message: 'Verify your identity by connecting with Facebook'},
-        {title: 'email', message: 'Enable account recovery by verifying your email address'},
-        {title: 'send', message: 'Send stellars to someone'},
-        {title: 'wallet', message: 'Create a new wallet'}
+    controller: function ($scope, session, bruteRequest, API_LOCATION) {
+      $scope.showRewards = false;
+
+      var rewardStatusTypes = ['pending', 'complete'];
+
+      $scope.rewardStatusIcons = {
+        'incomplete': 'glyphicon glyphicon-lock',
+        'pending': 'glyphicon glyphicon-time',
+        'complete': 'glyphicon glyphicon-ok-circle'
+      };
+
+      $scope.rewards = [
+        {message: 'Verify your identity by connecting with Facebook', status: 'incomplete'},
+        {message: 'Enable account recovery by verifying your email address', status: 'incomplete'},
+        {message: 'Send stellars to someone', status: 'incomplete'},
+        {message: 'Create a new wallet', status: 'complete'}
       ];
 
-      // TODO: Load the user's completed rewards from the RewardsGiven table.
-      var rewardsGiven = ['wallet'];
+      var rewardsRequest = new bruteRequest({
+          url: API_LOCATION + '/user/rewards',
+          type: 'GET',
+          dataType: 'json'
+      });
 
-      rewardTypes.forEach(function(reward){
-        if(rewardsGiven.indexOf(reward.title) == -1) $scope.incompleteRewards.push(reward);
-        else $scope.completeRewards.push(reward);
-      })
+      // Load the status of the user's rewards.
+      rewardsRequest.send(
+        {
+          username: session.get('username'),
+          updateToken: session.get('blob').get('updateToken')
+        },
+        //Success
+        function(rewardsGiven){
+          var rewardsComplete = 0;
+
+          // Update the status of the user's rewards.
+          rewardsGiven.forEach(function(reward){
+            // Rewards are 1-based indexed in the server.
+            $scope.rewards[reward.rewardID - 1].status = rewardStatusTypes[reward.status];
+            if(reward.status === 1) rewardsComplete++;
+          });
+
+          // Only show the rewards pane if there are rewards left to complete.
+          if(rewardsComplete != 3) $scope.showRewards = true;
+        }
+      );
     }
   };
 });
