5df7660cc1bace8c8d677a5fe8456e59af6ba9a5
Jared Deckard <jared.deckard@gmail.com>
2014-05-20 15:07:38 -0700

    Catch sass error to stop gulp from crashing

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,11 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
     return gulp.src('app/styles/main.scss')
-        .pipe($.rubySass({
-            style: 'expanded'
-        }))
+        .pipe($.rubySass({style: 'expanded'}))
         .pipe($.autoprefixer('last 1 version'))
         .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
+        .pipe($.size())
+        .on('error', console.log);
 });
 
 gulp.task('scripts', function () {
