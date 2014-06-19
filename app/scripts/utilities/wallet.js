f8d56dae7e31531aac7e1efbbe413135c531391f
Jared Deckard <jared.deckard@gmail.com>
2014-06-19 11:58:12 -0700

    Move to CCM cipher mode

--- app/scripts/utilities/wallet.js
+++ app/scripts/utilities/wallet.js
@@ -82,7 +82,8 @@ Wallet.SETTINGS = {
     SIZE: 256
   },
 
-  CIPHER_NAME: 'aes'
+  CIPHER_NAME: 'aes',
+  MODE: 'ccm'
 };
 
 /**
@@ -149,37 +150,20 @@ Wallet.encryptData = function(data, key) {
   // Initialize the cipher algorithm with the key.
   var cipher = new sjcl.cipher[Wallet.SETTINGS.CIPHER_NAME](key);
 
-  // SJCL doesn't know we are using HMAC-SHA256 to check integrity.
-  // TODO: Consider using GCM mode to avoid implementation errors.
-  sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
-
   // Encrypt the blob data in CBC mode using AES and a random 128bit IV.
   var rawIV = sjcl.random.randomWords(4);
-  var rawEncryptedText = sjcl.mode.cbc.encrypt(cipher, rawData, rawIV);
+  var rawCipherText = sjcl.mode[Wallet.SETTINGS.MODE].encrypt(cipher, rawData, rawIV);
 
-  // Concatenate the IV and encrypted blob data respectively into the cipher text.
-  var rawCipherText = rawIV.concat(rawEncryptedText);
+  // Base 64 encode.
+  var IV = sjcl.codec.base64.fromBits(rawIV);
   var cipherText = sjcl.codec.base64.fromBits(rawCipherText);
-
-  // Expand the key into a randomly salted hash key using PBKDF2.
-  var rawHashSalt = sjcl.random.randomWords(4);
-  var rawHashKey = sjcl.misc.pbkdf2(
-    key,
-    rawHashSalt,
-    Wallet.SETTINGS.PBKDF2.iterations,
-    Wallet.SETTINGS.PBKDF2.size
-  );
-  var hashKey = sjcl.codec.base64.fromBits(rawHashKey);
-
-  // Calculate the authentication hash of the cipher text using HMAC-SHA256.
-  var rawHash = CryptoJS.HmacSHA256(cipherText, hashKey).words;
 
   // Pack the results into a JSON encoded string.
   var resultString = JSON.stringify({
-    hash: sjcl.codec.base64.fromBits(rawHash),
-    hashSalt: sjcl.codec.base64.fromBits(rawHashSalt),
-    cipherText: sjcl.codec.base64.fromBits(rawCipherText),
-    cipherName: Wallet.SETTINGS.CIPHER_NAME
+    IV: IV,
+    cipherText: cipherText,
+    cipherName: Wallet.SETTINGS.CIPHER_NAME,
+    mode: Wallet.SETTINGS.MODE
   });
 
   // Encode the JSON string as base64 to obscure it's structure.
@@ -197,51 +181,25 @@ Wallet.decryptData = function(encryptedData, key) {
     // Parse the base64 encoded JSON object.
     var resultObject = JSON.parse(atob(encryptedData));
 
-    // Extract the authentication hash and hash salt from the encrypted data.
-    var rawGivenHash = sjcl.codec.base64.toBits(resultObject.hash);
-    var rawHashSalt = sjcl.codec.base64.toBits(resultObject.hashSalt);
-
     // Extract the cipher text from the encrypted data.
     var rawCipherText = sjcl.codec.base64.toBits(resultObject.cipherText);
-    var cipherText = sjcl.codec.base64.fromBits(rawCipherText);
+
+    // Extract the cipher text from the encrypted data.
+    var rawIV = sjcl.codec.base64.toBits(resultObject.IV);
 
     // Extract the cipher name from the encrypted data.
     var cipherName = resultObject.cipherName;
+    var mode = resultObject.mode;
   } catch(e) {
     // The encoded data does not represent valid base64 values.
     throw('Data corrupt!');
   }
 
-  // Expand the key into a hash key using PBKDF2 with the given hash salt.
-  var rawHashKey = sjcl.misc.pbkdf2(
-    key,
-    rawHashSalt,
-    Wallet.SETTINGS.PBKDF2.iterations,
-    Wallet.SETTINGS.PBKDF2.size
-  );
-  var hashKey = sjcl.codec.base64.fromBits(rawHashKey);
-
-  // Calculate the authentication hash of the cipher text using HMAC-SHA256.
-  var rawCalculatedHash = CryptoJS.HmacSHA256(cipherText, hashKey).words;
-  var calculatedHash = sjcl.codec.base64.fromBits(rawCalculatedHash);
-
-  // Ensure that the given cipher text hash matches the calculated cipher text hash.
-  var givenHash = sjcl.codec.base64.fromBits(rawGivenHash);
-  if(givenHash !== calculatedHash) throw("Message integrity check failed!");
-
-  // Extract the IV and encrypted data from the cipher text.
-  var rawIV = rawCipherText.slice(0, 4);
-  var rawEncryptedText = rawCipherText.slice(4);
-
-  // SJCL doesn't know we are using HMAC-SHA256 to check integrity.
-  // TODO: Consider using GCM mode to avoid implementation errors.
-  sjcl.beware["CBC mode is dangerous because it doesn't protect message integrity."]();
-
   // Initialize the cipher algorithm with the key.
   var cipher = new sjcl.cipher[cipherName](key);
 
   // Decrypt the data in CBC mode using AES and the given IV.
-  var rawData = sjcl.mode.cbc.decrypt(cipher, rawEncryptedText, rawIV);
+  var rawData = sjcl.mode[mode].decrypt(cipher, rawCipherText, rawIV);
   var data = sjcl.codec.utf8String.fromBits(rawData);
 
   // Parse and return the decrypted data as a JSON object.
