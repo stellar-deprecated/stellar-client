0e8b36388ad6e51f2b1457c243957b2ec98ba920
Scott Fleckenstein <nullstyle@gmail.com>
2014-06-20 15:51:23 -0700

    Renames $idAccountLoad to walletAddressLoaded

--- app/scripts/controllers/app-controller.js
+++ app/scripts/controllers/app-controller.js
@@ -5,7 +5,7 @@ var sc = angular.module('stellarClient');
     When we are connected to the stellard and we have our account info back from the wallet server,
     Ask the stellard for the account's info
     waits for:
-     $idAccountLoad
+     walletAddressLoaded
  */
 sc.controller('AppCtrl', ['$scope','$rootScope','stNetwork', function($scope, $rootScope, $network) {
 
@@ -80,7 +80,7 @@ sc.controller('AppCtrl', ['$scope','$rootScope','stNetwork', function($scope, $r
             .on('error', handleOffersError).request();
     };
 
-    $scope.$on('$idAccountLoad', function (e, data) {
+    $scope.$on('walletAddressLoaded', function (e, data) {
         $rootScope.account=data.account;
 
         // Server is connected
