2a276003dcc024f9445f704e6758aaf5fc397be0
Scott Fleckenstein <nullstyle@gmail.com>
2014-06-27 08:10:21 -0700

    Adds docs

--- app/scripts/utilities/singletonPromise.js
+++ app/scripts/utilities/singletonPromise.js
@@ -1,3 +1,21 @@
+/** 
+ * A singletonPromise ensures that an async operation does not have multiple 
+ * in-flight requests pending to it.  The normal use case for a singletonPromise
+ * is to prevent double form submission.  In addition, the singletonPromise 
+ * provides a mechanism (isLoading()) that lets you query the state of the promise,
+ * allowing for external entities to change their behavior (such as showing/hiding
+ * a loading indicator) based upon the promise's state.
+ * 
+ * Examples:
+ * 
+ *     $scope.attemptLogin = singletonPromise(function() {
+ *       $scope.loginError = null;
+ *       return deriveId().then(performLogin);
+ *     });
+ * 
+ * @param {Function} fun that will be serialized through the singletonPromise
+ * @return {Function} a function that starts the operation, if not already running
+ */
 angular.module('singletonPromise', []).value('singletonPromise', function (fun) {
   var loading = false;
   var result = function() {
@@ -14,6 +32,12 @@ angular.module('singletonPromise', []).value('singletonPromise', function (fun) 
     }
   }
 
+  /**
+   * Returns whether the wrapped operation is in progress
+   *
+   * @return {Boolean} true if in progress
+   *
+   */
   result.isLoading = function() {
     return loading;
   }
