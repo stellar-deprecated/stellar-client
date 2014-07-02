b1fbb1f30ab619d6ef929e1f7c0302ec0253cce5
Jared Deckard <jared.deckard@gmail.com>
2014-07-01 20:35:15 -0700

    Make transaction history responsive

--- app/scripts/controllers/transaction-history.js
+++ app/scripts/controllers/transaction-history.js
@@ -2,28 +2,10 @@
 
 var sc = angular.module('stellarClient');
 
-sc.controller('TransactionHistoryCtrl', ['$scope', 'stNetwork', 'ngTableParams', '$filter', 'session', function( $scope, $network, ngTableParams, $filter, session)
+sc.controller('TransactionHistoryCtrl', ['$scope', function($scope)
 {
   $scope.typeIcons = {
     'sent': 'glyphicon glyphicon-upload',
     'received': 'glyphicon glyphicon-download'
   };
-
-  $scope.tableParams = new ngTableParams({
-    page: 1,
-    count: 10,
-    sorting: {
-      date: 'desc'
-    }
-  }, {
-    total: $scope.history.length,
-    getData: function($defer, params) {
-      var data = params.sorting() ? $filter('orderBy')($scope.history, params.orderBy()) : $scope.history;
-      $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-    }
-  });
-
-  $scope.$on('$paymentNotification', function(){
-    $scope.tableParams.reload();
-  });
 }]);
