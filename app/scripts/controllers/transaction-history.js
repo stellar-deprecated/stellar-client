47237ef37db9019a506a596f19d1271ff651f3e8
Jared Deckard <jared.deckard@gmail.com>
2014-07-08 11:19:46 -0700

    Add unstyled ngGrid for the transaction history

--- app/scripts/controllers/transaction-history.js
+++ app/scripts/controllers/transaction-history.js
@@ -2,10 +2,51 @@
 
 var sc = angular.module('stellarClient');
 
-sc.controller('TransactionHistoryCtrl', ['$scope', function($scope)
+sc.controller('TransactionHistoryCtrl', function($scope)
 {
   $scope.typeIcons = {
     'sent': 'glyphicon glyphicon-upload',
     'received': 'glyphicon glyphicon-download'
   };
-}]);
+
+  function sortAmount(a, b){
+    return a.to_number() - b.to_number();
+  }
+
+  $scope.transactionGrid = {
+    data: 'history',
+    enableRowSelection: false,
+    plugins: [new ngGridFlexibleHeightPlugin()],
+    columnDefs: [
+      {
+        field: 'transaction.type',
+        displayName: 'Type',
+        cellTemplate: '<i ng-class="typeIcons[row.getProperty(col.field)]"></i> ' +
+                      '<span>{{ row.getProperty(col.field) }}</span>'
+      },
+      {
+        field: 'transaction.amount',
+        displayName: 'Amount',
+        cellTemplate: '<span>{{ row.getProperty(col.field).to_human() }} STR</span>',
+        sortFn: function(a, b){
+          return a.to_number() - b.to_number();
+        }
+      },
+      {
+        field: 'date',
+        displayName: 'Date',
+        cellTemplate: '<span am-time-ago="row.getProperty(col.field)"></span>'
+      },
+      {
+        field: 'transaction.type',
+        displayName: '',
+        cellTemplate: '<span>{{ row.getProperty(col.field) === "sent" ? "to" : "from" }}</span>'
+      },
+      {
+        field: 'transaction.counterparty',
+        displayName: '',
+        cellTemplate: '<span class="address">{{ row.getProperty(col.field) | addressToUsername }}</span>'
+      }
+    ]
+  };
+});
