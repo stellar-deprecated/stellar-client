4d2a73796cff71325a05e6868b1e873fad9ac244
Jared Deckard <jared.deckard@gmail.com>
2014-07-08 13:42:32 -0700

    Style ngGrid for the transaction history

--- app/scripts/controllers/transaction-history.js
+++ app/scripts/controllers/transaction-history.js
@@ -17,16 +17,21 @@ sc.controller('TransactionHistoryCtrl', function($scope)
     data: 'history',
     enableRowSelection: false,
     plugins: [new ngGridFlexibleHeightPlugin()],
+    headerRowHeight: '70',
+    rowHeight: '70',
+    rowTemplate: '<div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell {{col.cellClass}}"><div class="ngVerticalBar" ng-style="{height: rowHeight}">&nbsp;</div><div ng-cell></div></div>',
     columnDefs: [
       {
         field: 'transaction.type',
         displayName: 'Type',
+        width: '150',
         cellTemplate: '<i ng-class="typeIcons[row.getProperty(col.field)]"></i> ' +
-                      '<span>{{ row.getProperty(col.field) }}</span>'
+                      '<span class="tx-type">{{ row.getProperty(col.field) }}</span>'
       },
       {
         field: 'transaction.amount',
         displayName: 'Amount',
+        width: '20%',
         cellTemplate: '<span>{{ row.getProperty(col.field).to_human() }} STR</span>',
         sortFn: function(a, b){
           return a.to_number() - b.to_number();
@@ -35,16 +40,19 @@ sc.controller('TransactionHistoryCtrl', function($scope)
       {
         field: 'date',
         displayName: 'Date',
+        width: '20%',
         cellTemplate: '<span am-time-ago="row.getProperty(col.field)"></span>'
       },
       {
         field: 'transaction.type',
         displayName: '',
-        cellTemplate: '<span>{{ row.getProperty(col.field) === "sent" ? "to" : "from" }}</span>'
+        width: '60',
+        cellTemplate: '<span class="tx-direction">{{ row.getProperty(col.field) === "sent" ? "to" : "from" }}</span>'
       },
       {
         field: 'transaction.counterparty',
         displayName: '',
+        width: '35%',
         cellTemplate: '<span class="address">{{ row.getProperty(col.field) | addressToUsername }}</span>'
       }
     ]
