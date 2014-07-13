aadfa6c89f720da69cdafb907cd7cbb63310f824
Jared Deckard <jared.deckard@gmail.com>
2014-07-13 16:31:58 -0700

    Generate signing keys from stellar-lib seed

---
+++ app/scripts/utilities/signingKeys.js
@@ -0,0 +1,21 @@
+/**
+ * Generates a random stellar wallet or recovers a stellar wallet from its seed.
+ *
+ * @param {string} [seed] The optional base58 encoded seed.
+ * @returns {{seed: {string}, address: {string}}}
+ */
+var SigningKeys = function(seed){
+  if(seed){
+    seed = new stellar.Seed().parse_json(seed);
+  } else {
+    seed = new stellar.Seed().random();
+  }
+
+  var key = seed.get_key();
+  var address = key.get_address();
+
+  return {
+    secret: seed.to_json(),
+    address: address.to_json()
+  }
+};
