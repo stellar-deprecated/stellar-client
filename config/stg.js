538fe5e0ebb81af1945641a46ce0cfc39f9fcf4a
Scott Fleckenstein <nullstyle@gmail.com>
2014-07-23 15:52:52 -0700

    Update environment names and add stg environment

---
+++ config/stg.js
@@ -0,0 +1,40 @@
+var Options = {
+    server: {
+        "trusted" : true,
+        "websocket_ip" : "public-01-stg.gostellar.org",
+        "websocket_port" : 9001,
+        "websocket_ssl" : true
+    },
+
+    mixpanel: {
+        "token": '',
+        // Don't track events by default
+        "track": false
+    },
+
+    stellar_contact: {
+        destination: "StellarFoundation",
+        destination_address: "gpjh5ZsnpfLy1FYD8V9b7oNbwZqXMFF7Ha",
+        domain: "gostellar.org",
+        type: "federation_record"
+    },
+
+    INFLATION_DEST: 'g4eRqgZfzfj3132y17iaf2fp6HQj1gofjt',
+
+    APP_ID: '1512347158994532',
+    DOMAIN_NAME: 'stg.gostellar.org',
+    DEFAULT_FEDERATION_DOMAIN: 'stg.gostellar.org',
+    API_SERVER: 'https://api-stg.gostellar.org',
+    WALLET_SERVER: 'https://wallet-stg.gostellar.org',
+
+    // If set, login will persist across sessions (page reload). This is mostly
+    // intended for developers, be careful about using this in a real setting.
+    PERSISTENT_SESSION : false,
+    ALPHA_PHASE : true,
+    IDLE_LOGOUT_TIMEOUT : 15 * 60 * 1000, //15 minutes
+
+    REPORT_ERRORS : true,
+
+    // Number of transactions each page has in balance tab notifications
+    transactions_per_page: 50
+};
