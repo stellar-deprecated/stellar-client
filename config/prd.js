4faa636a8faec8006361455f5efa694828bcab72
Elliot <elliot@kraken.com>
2014-08-01 23:12:26 -0700

    Change gostellar to stellar

--- config/prd.js
+++ config/prd.js
@@ -15,7 +15,7 @@ var Options = {
     stellar_contact: {
         destination: "StellarFoundation",
         destination_address: "gpjh5ZsnpfLy1FYD8V9b7oNbwZqXMFF7Ha",
-        domain: "gostellar.org",
+        domain: "stellar.org",
         type: "federation_record"
     },
 
