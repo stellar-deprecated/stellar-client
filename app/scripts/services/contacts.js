6b9e61178ac5ad43d071591abaed869d603236f2
Jared Deckard <jared.deckard@gmail.com>
2014-08-06 13:20:44 -0700

    Save the contacts to localStorage with date cached

--- app/scripts/services/contacts.js
+++ app/scripts/services/contacts.js
@@ -3,18 +3,42 @@
 var sc = angular.module('stellarClient');
 
 sc.service('contacts', function(rpReverseFederation) {
-  var contactsByAddress = {};
-  var contactsByFederatedName = {};
+  var contactsByAddress;
+  var contactsByFederatedName;
+
+  // Try to load the contact list from localStorage.
+  try {
+    if (localStorage.stellarContactsByAddress) {
+      contactsByAddress = JSON.parse(localStorage.stellarContactsByAddress);
+    }
+    if (localStorage.stellarContactsByFederatedName) {
+      contactsByFederatedName = JSON.parse(localStorage.stellarContactsByFederatedName);
+    }
+  } catch(err) {
+    // Unable to access localStorage.
+  }
+
+  contactsByAddress = contactsByAddress || {};
+  contactsByFederatedName = contactsByFederatedName || {};
 
   /**
    * If the address is not in the contact list, try to create a contact by
    * reverse federating the address.
    */
   function addContact(federatedContact) {
+    federatedContect.dateCached = Date.now();
     contactsByAddress[federatedContact.destination_address] = federatedContact;
 
     var federatedName = federatedContact.destination + '@' + federatedContact.domain;
     contactsByFederatedName[federatedName] = federatedContact;
+
+    // Try to save the contact list to localStorage.
+    try {
+      localStorage.stellarContactsByAddress = JSON.stringify(contactsByAddress);
+      localStorage.stellarContactsByFederatedName = JSON.stringify(contactsByFederatedName);
+    } catch(err) {
+      // Unable to access localStorage.
+    }
   }
 
   /**
