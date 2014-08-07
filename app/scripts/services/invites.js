dd1441110d97e77c821355b564bab7a8549d7fbd
Andrew Rogers <abennobashi@gmail.com>
2014-08-06 18:55:46 -0700

    move data param into each function

--- app/scripts/services/invites.js
+++ app/scripts/services/invites.js
@@ -3,23 +3,31 @@
 var sc = angular.module('stellarClient');
 
 sc.service('invites', function($http, session) {
-    var data = {
-        username: session.get('username'),
-        updateToken: session.get('wallet').keychainData.updateToken
-    };
     return {
         send: function (email) {
-            data.email = email;
+            var data = {
+                username: session.get('username'),
+                updateToken: session.get('wallet').keychainData.updateToken,
+                email: email
+            };
 
             return $http.post(Options.API_SERVER + "/invites/send", data);
         },
         cancel: function (invite) {
-            data.inviteId = invite.inviteId;
+            var data = {
+                username: session.get('username'),
+                updateToken: session.get('wallet').keychainData.updateToken,
+                inviteId: invite.inviteId
+            };
 
             return $http.post(Options.API_SERVER + "/invites/cancel", data);
         },
         resend: function (invite) {
-            data.inviteId = invite.inviteId;
+            var data = {
+                username: session.get('username'),
+                updateToken: session.get('wallet').keychainData.updateToken,
+                inviteId: invite.inviteId
+            };
 
             return $http.post(Options.API_SERVER + "/invites/resend", data);
         }
