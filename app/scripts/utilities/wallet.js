e3b6a9f3bf082cfaa8a6117a3883761e56de8b08
Jared Deckard <jared.deckard@gmail.com>
2014-08-12 16:20:00 -0700

    Convert milliseconds to seconds

--- app/scripts/utilities/wallet.js
+++ app/scripts/utilities/wallet.js
@@ -195,7 +195,7 @@ angular.module('stellarClient').factory('Wallet', function($q, $http, ipCookie) 
     var encryptedWalletKey = Wallet.encryptData(this.key, loginWalletKey);
 
     catchAndSwallowSecurityErrors(function() {
-      ipCookie("localWalletKey", loginWalletKey, {expires:Options.IDLE_LOGOUT_TIMEOUT, expirationUnit:'seconds'});
+      ipCookie("localWalletKey", loginWalletKey, {expires:Options.IDLE_LOGOUT_TIMEOUT/1000, expirationUnit:'seconds'});
       localStorage.encryptedWalletKey = encryptedWalletKey;
       localStorage.wallet             = Wallet.encryptData(self, sjcl.codec.hex.toBits(self.key));
       sessionStorage.wallet           = JSON.stringify(self);
@@ -206,7 +206,7 @@ angular.module('stellarClient').factory('Wallet', function($q, $http, ipCookie) 
   Wallet.prototype.bumpLocalTimeout = function() {
     //TODO: push the cookie timeout foreward
     catchAndSwallowSecurityErrors(function() {
-      ipCookie("localWalletKey", ipCookie("localWalletKey"), {expires:Options.IDLE_LOGOUT_TIMEOUT, expirationUnit:'seconds'});
+      ipCookie("localWalletKey", ipCookie("localWalletKey"), {expires:Options.IDLE_LOGOUT_TIMEOUT/1000, expirationUnit:'seconds'});
     });
   }
 
