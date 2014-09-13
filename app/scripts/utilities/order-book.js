06ef58c6939dda8a42a4f5a5793180a03b09b059
Jared Deckard <jared.deckard@gmail.com>
2014-09-12 22:15:44 -0700

    Sort by price numerically not alphabetically

--- app/scripts/utilities/order-book.js
+++ app/scripts/utilities/order-book.js
@@ -30,6 +30,12 @@ angular.module('stellarClient').factory('OrderBook', function($q, $rootScope, Tr
         return v.toString();
       });
     }
+    function sortAscending(a, b) {
+      return new BigNumber(a.price).cmp(b.price);
+    }
+    function sortDescending(a, b) {
+      return sortAscending(b, a);
+    }
 
     var merged = _(priceLevels)
       .map(toBigNumber)
@@ -50,15 +56,18 @@ angular.module('stellarClient').factory('OrderBook', function($q, $rootScope, Tr
           amount:     totalAmount,
           totalValue: totalValue,
         };
-      });
+      })
+      .value();
 
-    var sorted = merged.sortBy('price');
+    var sorted;
 
-    if(!ascending) {
-      sorted = sorted.reverse();
+    if(ascending) {
+      sorted = merged.sort(sortAscending);
+    } else {
+      sorted = merged.sort(sortDescending);
     }
         
-    var result = sorted.map(toString).value();
+    var result = sorted.map(toString);
 
 
     return result;
