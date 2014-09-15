2fa88bdfdb701b9c49de19bbeb4ca3adc02faa94
Bartek Nowotarski <bartek@nowotarski.info>
2014-09-15 22:13:46 +0200

    Fixed minor bug in AddGatewayCtrl

--- app/scripts/controllers/add-gateway-controller.js
+++ app/scripts/controllers/add-gateway-controller.js
@@ -20,6 +20,7 @@ sc.controller('AddGatewayCtrl', function($scope, $q, session, singletonPromise, 
 
   $scope.loadCurrencies = singletonPromise(function (){
     $scope.searchStatus = 'loading';
+    $scope.lastGatewaySearch = $scope.gatewaySearch;
 
     if($scope.gateways[$scope.gatewaySearch]) {
       $scope.searchStatus = 'already_added';
