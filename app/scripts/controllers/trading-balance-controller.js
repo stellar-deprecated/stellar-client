0c4153dd007a14e0a6867f8806c155e7fe4e1abd
irisli <_l@berkeley.edu>
2014-09-23 16:07:54 -0700

    Fetch account balance in the trading balance widget

--- app/scripts/controllers/trading-balance-controller.js
+++ app/scripts/controllers/trading-balance-controller.js
@@ -2,6 +2,36 @@
 
 var sc = angular.module('stellarClient');
 
-sc.controller('TradingBalanceCtrl', function($scope) { 
-  
+sc.controller('TradingBalanceCtrl', function($scope, $rootScope, $filter, AccountLines) {
+  $scope.accountLines = [];
+
+  $scope.getCurrencyBalance = function(currency) {
+    if (currency.currency === 'STR') {
+      return $filter('rpamount')($rootScope.balance);
+    }
+
+    var accountLine = _.find($scope.accountLines, {
+      'currency': currency.currency,
+      'account': currency.issuer
+    });
+
+    if (accountLine) {
+      return accountLine.balance;
+    } else {
+      return 0;
+    }
+  };
+
+  function getAccountLines() {
+    AccountLines.get()
+      .then(function(accountLines) {
+        $scope.accountLines = accountLines;
+      });
+  }
+
+  $scope.$on('AccountLines:update', function(event, accountLines) {
+      $scope.accountLines = accountLines;
+  });
+
+  getAccountLines();
 });
