aa202a830bd09b4c9124b2b9fc1f076c805aefcd
irisli <_l@berkeley.edu>
2014-09-29 12:03:43 -0700

    Added a pair-not-selected state on the trading balances widget

--- app/scripts/controllers/trading-balance-controller.js
+++ app/scripts/controllers/trading-balance-controller.js
@@ -5,22 +5,27 @@ var sc = angular.module('stellarClient');
 sc.controller('TradingBalanceCtrl', function($scope, $q, Balances) {
   $scope.baseBalance = null;
   $scope.counterBalance = null;
-
-  $scope.balancesLoaded = false;
+  $scope.state = 'loading';
 
   $scope.$on('balances:update', function(event, accountLines) {
     refreshBalances();
   });
 
   function refreshBalances() {
+    if (!$scope.formData.baseCurrency.currency || !$scope.formData.counterCurrency.currency) {
+      $scope.state = 'pair-not-selected';
+      return;
+    } else {
+      $scope.state = 'loading';
+    }
+
     $q.all([
       Balances.get($scope.formData.baseCurrency),
       Balances.get($scope.formData.counterCurrency),
     ]).then(function(results) {
-
       $scope.baseBalance    = results[0];
       $scope.counterBalance = results[1];
-      $scope.balancesLoaded = true;
+      $scope.state = 'available';
     });
   }
 
