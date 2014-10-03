2716e44e81eec1b9959ed75dbd8fcde8bfaa522e
Scott Fleckenstein <nullstyle@gmail.com>
2014-10-03 11:30:52 -0700

    De-lint add-email-controller.js

--- app/scripts/controllers/add-email-controller.js
+++ app/scripts/controllers/add-email-controller.js
@@ -1,3 +1,6 @@
+var sc = angular.module('stellarClient');
+/* global addEmailForm */
+
 sc.controller('AddEmailCtrl', function ($scope, $rootScope, $http, $state, session) {
   $scope.loading = false;
   $scope.errors = [];
@@ -24,40 +27,41 @@ sc.controller('AddEmailCtrl', function ($scope, $rootScope, $http, $state, sessi
       .success(addEmailSuccess)
       .error(addEmailError)
       .finally(addEmailDone);
+    }
+    
+    
+    function addEmailSuccess(response) {
+      // Store the email address in the wallet.
+      wallet.mainData.email = $scope.email;
+      session.syncWallet('update');
 
-      function addEmailSuccess(response) {
-        // Store the email address in the wallet.
-        wallet.mainData.email = $scope.email;
-        session.syncWallet('update');
+      // Switch to the verify overlay.
+      $scope.rewards[2].status = "unverified";
 
-        // Switch to the verify overlay.
-        $scope.rewards[2].status = "unverified";
+      return session.getUser().refresh();
+    }
 
-        return session.getUser().refresh();
-      }
-
-      function addEmailError(response) {
-        if (response && response.status == 'fail') {
-          switch (response.code) {
-            case 'invalid_update_token':
-              // this user's update token is invalid, send to login
-              $state.transitionTo('login');
-              break;
-            case 'already_taken':
-              $scope.errors.push("This email is already taken.");
-              break;
-            default:
-              $scope.errors.push("Server error.");
-          }
-        } else {
-          $scope.errors.push("Server error.");
+    function addEmailError(response) {
+      if (response && response.status === 'fail') {
+        switch (response.code) {
+          case 'invalid_update_token':
+            // this user's update token is invalid, send to login
+            $state.transitionTo('login');
+            break;
+          case 'already_taken':
+            $scope.errors.push("This email is already taken.");
+            break;
+          default:
+            $scope.errors.push("Server error.");
         }
-        $scope.loading = false;
+      } else {
+        $scope.errors.push("Server error.");
       }
+      $scope.loading = false;
+    }
 
-      function addEmailDone() {
-        $scope.loading = false;
-      }
+    function addEmailDone() {
+      $scope.loading = false;
     }
   };
 
