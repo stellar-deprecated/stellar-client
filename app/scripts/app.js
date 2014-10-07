5e43fdefb0701669797d67633397f4ddafdf853c
Jared Deckard <jared.deckard@gmail.com>
2014-10-07 12:53:28 -0700

    Remove login event hack

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -168,15 +168,7 @@ stellarClient.run(function($rootScope, $state, $timeout, ipCookie, session, Flas
       }
 
       if(wallet) {
-        // Login with the local wallet and continue to the requested state.
-        event.preventDefault();
-
-        // HACK: The controllers in ng-included templates have not initialized yet.
-        //       Apply a $timeout so they have time to listen for login events.
-        $timeout(function() {
-          session.login(wallet);
-          $state.transitionTo(toState);
-        }, 0);
+        session.login(wallet);
       } else if(toState.authenticate) {
         // Redirect authenticated routes to login if we are unable to login from local.
         event.preventDefault();
