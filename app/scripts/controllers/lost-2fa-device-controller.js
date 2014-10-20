9854ae94729f7060159f74505b6523c8ddec34a3
Bartek Nowotarski <bartek@nowotarski.info>
2014-10-20 19:50:52 +0200

    Lost device finished

--- app/scripts/controllers/lost-2fa-device-controller.js
+++ app/scripts/controllers/lost-2fa-device-controller.js
@@ -1,5 +1,33 @@
 'use strict';
 
-angular.module('stellarClient').controller('Lost2FADeviceCtrl', function($scope) {
-  //
+angular.module('stellarClient').controller('Lost2FADeviceCtrl', function($scope, singletonPromise) {
+  $scope.error = null;
+  $scope.sent = false;
+
+  $scope.sendRequest = function($event) {
+    $event.preventDefault();
+    $scope.asyncRequest();
+  };
+
+  $scope.asyncRequest = singletonPromise(function() {
+    $scope.error = null;
+
+    StellarWallet.lostTotpDevice({
+      server: Options.WALLET_SERVER+'/v2',
+      username: $scope.username+'@stellar.org',
+      password: $scope.password
+    }).then(function() {
+      $scope.sent = true;
+    }).catch(StellarWallet.errors.WalletNotFound, function() {
+      $scope.sent = true;
+    }).catch(StellarWallet.errors.MissingField, function() {
+      $scope.error = 'Please fill in all fields.';
+    }).catch(StellarWallet.errors.ConnectionError, function() {
+      $scope.error = 'Problems connecting wallet server. Please try again later.';
+    }).catch(StellarWallet.errors.UnknownError, function() {
+      $scope.error = 'Unknown error. Please try again later.';
+    }).finally(function() {
+      $scope.$apply();
+    });
+  });
 });
