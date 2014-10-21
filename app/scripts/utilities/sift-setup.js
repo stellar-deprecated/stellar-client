b51764bf8b5ecab23e5eea458b1cf87469e01ef9
Jared Deckard <jared.deckard@gmail.com>
2014-10-21 13:03:04 -0700

    Delint sift setup script

--- app/scripts/utilities/sift-setup.js
+++ app/scripts/utilities/sift-setup.js
@@ -1,17 +1,14 @@
-var _user_id = '';
 var _sift = _sift || [];
 
-function loadSiftScript(userId) {
-  _user_id = userId;
-
+window.loadSiftScript = function(userId) {
   _sift.push(['_setAccount', Options.SIFT_SCIENCE_ACCOUNT]);
-  _sift.push(['_setUserId', _user_id]);
+  _sift.push(['_setUserId', userId]);
   _sift.push(['_trackPageview']);
 
   var e = document.createElement('script');
   e.type = 'text/javascript';
   e.async = true;
-  e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
+  e.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(e, s);
-}
+};
