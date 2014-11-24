76f6b4361c8d72829ad1d4cb8c4db98344d584c0
Bartek Nowotarski <bartek@nowotarski.info>
2014-11-24 13:41:54 +0100

    Mark browsers without crypto unsupported

--- app/scripts/utilities/signingKeys.js
+++ app/scripts/utilities/signingKeys.js
@@ -11,7 +11,6 @@ var SigningKeys = function(seed){
   if(seed){
     seed = new stellar.Seed().parse_json(seed);
   } else {
-    Util.ensureEntropy();
     seed = new stellar.Seed().random();
   }
 
