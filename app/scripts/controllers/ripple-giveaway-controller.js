2f4e7f401729079cda8a3e63dd8152f702dabcd6
Bartek Nowotarski <bartek@nowotarski.info>
2014-12-10 20:32:17 +0100

    Ripple giveaway CSS

--- app/scripts/controllers/ripple-giveaway-controller.js
+++ app/scripts/controllers/ripple-giveaway-controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module('stellarClient').controller('RippleGiveawayCtrl', function($scope, rippleGiveaway, debounce) {
+angular.module('stellarClient').controller('RippleGiveawayCtrl', function($scope, rippleGiveaway) {
   $scope.address = null;
   $scope.addressData = null;
+  $scope.error = null;
   $scope.option = 'pro_rata'; // Make this default option.
 
   rippleGiveaway.getClaimData()
@@ -17,14 +18,15 @@ angular.module('stellarClient').controller('RippleGiveawayCtrl', function($scope
       });
     });
 
-  var getAddress = function() {
-    return rippleGiveaway.getAddress($scope.address)
+  var getAddress = function(address) {
+    $scope.error = null;
+    return rippleGiveaway.getAddress(address)
       .success(function(response) {
         $scope.addressData = response.data;
       })
       .error(function(response) {
         if (response.error && response.error === 'not_found') {
-          //
+          $scope.error = 'This Ripple address is ineligible.';
         } else {
           Raven.captureMessage('Ripple Giveaway: getAddress() error response', {
             extra: {
@@ -38,22 +40,41 @@ angular.module('stellarClient').controller('RippleGiveawayCtrl', function($scope
       });
   };
 
-  $scope.$watch('address', function(newVal, oldVal) {
-    if (newVal === oldVal) {
-      return;
-    }
+  $scope.nextStep = function($event) {
+    /* jshint camelcase:false */
+    $event.preventDefault();
+    var previousStatus = $scope.addressData.status;
+    getAddress($scope.addressData.ripple_address)
+      .then(function() {
+        if (previousStatus === $scope.addressData.status) {
+          switch (previousStatus) {
+            case 'not_associated':
+              $scope.error = 'We haven\'t processed your payment yet. Please try again in a few minutes.';
+              break;
+            case 'donation':
+              $scope.error = 'We haven\'t processed your donation yet. Please try again in a few minutes.';
+              break;
+          }
+        }
+      });
+  };
+
+  $scope.getAddress = function($event) {
+    $event.preventDefault();
     $scope.loading = true;
     $scope.addressData = null;
-    debounce(getAddress, 1000)();
-  });
+    getAddress($scope.address);
+  };
 
-  $scope.claim = function() {
+  $scope.claim = function($event) {
+    $event.preventDefault();
     rippleGiveaway.claim($scope.address, $scope.option)
       .success(function() {
-        getAddress();
+        /* jshint camelcase:false */
+        getAddress($scope.addressData.ripple_address);
       })
       .error(function(response) {
-        // TODO error message
+        $scope.error = 'Error. Please try again later.';
         Raven.captureMessage('Ripple Giveaway: claim() error response', {
           extra: {
             response: response
