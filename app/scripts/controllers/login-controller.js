ac78ec5b55dfa2f61a56142926a5be9bfba32cfa
Shannon Romano <shannon@stellar.org>
2021-01-12 15:18:11 -0800

    Update app/scripts/controllers/login-controller.js
    Co-authored-by: Alex Cordeiro <accordeiro@users.noreply.github.com>

--- app/scripts/controllers/login-controller.js
+++ app/scripts/controllers/login-controller.js
@@ -60,7 +60,7 @@ angular.module('stellarClient').controller('LoginCtrl', function($rootScope, $sc
 
   FlashMessages.add({
     title: 'The Account Upgrade service is temporarily unavavilable',
-    info: 'We are activily working to restore it. In the meantime, please reach out to legacy@stellar.org if you need help',
+    info: 'We are actively working to restore it. In the meantime, please reach out to legacy@stellar.org if you need help',
     type: 'error',
     showCloseIcon: false,
   });
